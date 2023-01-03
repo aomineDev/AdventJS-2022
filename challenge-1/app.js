57ffd567394f856ae60b8e75fd9b158b818b2321
aomine <aomine1745@gmail.com>
2023-01-02 22:53:11 -0500

    update: challenge 1 completed

---
+++ challenge-1/app.js
@@ -0,0 +1,11 @@
+const gifts = ['cat', 'game', 'socks']
+const wrapped = wrapping(gifts)
+
+function wrapping(gifts) {
+  return gifts.map(g => '*'.repeat(g.length + 2)
+      + '\n*'
+      + g
+      + '*\n'
+      + '*'.repeat(g.length + 2)
+  )
+}
