9f3f8c7952e46658f12d237b516fbe2926ec8aa5
aomine <aomine1745@gmail.com>
2023-01-03 00:18:54 -0500

    update: challenge 17 completed

---
+++ challenge-17/app.js
@@ -0,0 +1,25 @@
+carryGifts(['game', 'bike', 'book', 'toy'], 10)
+
+carryGifts(['game', 'bike', 'book', 'toy'], 7)
+
+carryGifts(['game', 'bike', 'book', 'toy'], 4)
+
+carryGifts(['toy', 'gamme', 'toy', 'bike'], 6)
+
+function carryGifts(gifts, maxWeight) {
+  return gifts
+    .reduce((acc, curr) => {
+      const l = acc.length - 1,
+      w = curr.length
+
+      if (acc[l].w + w <= maxWeight) {
+        acc[l].w += w
+        acc[l].g.push(curr)
+      }
+      else if (w <= maxWeight) acc.push({ w, g: [curr] })
+
+      return acc
+    }, [{ w: 0, g: [] }])
+    .filter(e => e.w)
+    .map(e => e.g.join(' '))
+}
